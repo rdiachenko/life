9b91944cb638899a0f5d1caece80485acdcc66dd
Ruslan Diachenko <rd.ryly@gmail.com>
2015-10-01 22:11:12 +0100

    two dimentional array used instead of manual calculation of index in a single dimentional array

--- cpp-life/LifeModel.cpp
+++ cpp-life/LifeModel.cpp
@@ -1,13 +1,8 @@
-#include <iostream>
 #include "LifeModel.h"
-
-using std::cout;
-using std::endl;
 
 int LifeModel::getCell(int row, int col)
 {
-	int index = row * n + col;
-	return frontField[index];
+	return frontField[row][col];
 }
 
 void LifeModel::setCellOn(int row, int col)
@@ -22,38 +17,30 @@ void LifeModel::setCellOff(int row, int col)
 
 void LifeModel::setCell(int row, int col, int val)
 {
-	int index = row * n + col;
-	frontField[index] = val;
+	frontField[row][col] = val;
 }
 
 void LifeModel::simulate()
 {
-	cout << "simultaion step started" << endl;
-
-	for (int i = 0; i != n; i++)
+	for (int i = 0; i != n; ++i)
 	{
-		int row = i * n;
-
-		for (int j = 0; j != m; j++)
+		for (int j = 0; j != m; ++j)
 		{
 			int neighboursCount = countNeighbours(i, j);
 			int cellState = getCell(i, j);
-			int index = row + j;
 
 			if ((cellState == 0 && neighboursCount == 3)
 					|| (cellState > 0 && (neighboursCount == 2 || neighboursCount == 3)))
 			{
-				backField[index] = 1;
+				backField[i][j] = 1;
 			}
 			else
 			{
-				backField[index] = 0;
+				backField[i][j] = 0;
 			}
 		}
 	}
 	swapFields();
-
-	cout << "simultaion step finished" << endl;
 }
 
 int LifeModel::countNeighbours(int row, int col)
@@ -72,7 +59,7 @@ int LifeModel::countNeighbours(int row, int col)
 	};
 	int count = 0;
 
-	for (int k = 0; k != offsetsCount; k++)
+	for (int k = 0; k != offsetsCount; ++k)
 	{
 		int i = row + offsets[k][0];
 		int j = col + offsets[k][1];
@@ -87,22 +74,19 @@ int LifeModel::countNeighbours(int row, int col)
 
 void LifeModel::swapFields()
 {
-	int* temp = frontField;
+	int** temp = frontField;
 	frontField = backField;
 	backField = temp;
 }
 
 void LifeModel::clear()
 {
-	for (int i = 0; i != n; i++)
+	for (int i = 0; i != n; ++i)
 	{
-		int row = i * n;
-
-		for (int j = 0; j != m; j++)
+		for (int j = 0; j != m; ++j)
 		{
-			int index = row + j;
-			frontField[index] = 0;
-			backField[index] = 0;
+			frontField[i][j] = 0;
+			backField[i][j] = 0;
 		}
 	}
 }
--- cpp-life/LifeModel.h
+++ cpp-life/LifeModel.h
@@ -8,11 +8,22 @@ class LifeModel
 		{
 			n = width;
 			m = height;
-			frontField = new int[n * m];
-			backField = new int[n * m];
+			frontField = new int*[n];
+			backField = new int*[n];
+
+			for (int i = 0; i != n; ++i)
+			{
+				frontField[i] = new int[m];
+				backField[i] = new int[m];
+			}
 		}
 		~LifeModel()
 		{
+			for (int i = 0; i != n; ++i)
+			{
+				delete[] frontField[i];
+				delete[] backField[i];
+			}
 			delete[] frontField;
 			delete[] backField;
 		}
@@ -25,8 +36,8 @@ class LifeModel
 	private:
 		int n;
 		int m;
-		int* frontField;
-		int* backField;
+		int** frontField;
+		int** backField;
 		void setCell(int, int, int);
 		int countNeighbours(int, int);
 		void swapFields();
