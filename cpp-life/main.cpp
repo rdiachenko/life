fee2814903abff671c5690d6633dc5cf66f83f99
Ruslan Diachenko <rd.ryly@gmail.com>
2015-10-05 23:24:55 +0100

    main window was added

--- cpp-life/main.cpp
+++ cpp-life/main.cpp
@@ -1,33 +1,68 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 #include "LifeModel.h"
 
-using namespace std;
+using std::cerr;
+using std::endl;
 
-void println(LifeModel& model)
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* window = NULL;
+SDL_Surface* surface = NULL;
+
+bool init()
 {
-	for (int i = 0; i != 7; i++)
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		for (int j = 0; j != 7; j++)
-		{
-			cout << model.getCell(i, j);
-		}
-		cout << endl;
+		cerr << "SDL could not be initialized: " << SDL_GetError() << endl;
+		success = false;
 	}
+	else
+	{
+		window = SDL_CreateWindow("Life", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			cerr << "Window could not be created: " << SDL_GetError() << endl;
+			success = false;
+		}
+		else
+		{
+			surface = SDL_GetWindowSurface(window);
+			SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
+		}
+	}
+	return success;
 }
 
-int main()
+void close()
 {
-	int width = 7;
-	int height = 7;
-	LifeModel model(width, height);
-	model.setCellOn(2, 2);
-	model.setCellOn(2, 3);
-	model.setCellOn(2, 1);
+	SDL_DestroyWindow(window);
+	window = NULL;
+	SDL_Quit();
+}
 
-	println(model);
-	model.simulate();
-	println(model);
-	model.simulate();
-	println(model);
+int main(int argc, char** argv)
+{
+	if (init())
+	{
+		bool quit = false;
+		SDL_Event event;
+
+		while (!quit)
+		{
+			while (SDL_PollEvent(&event) != 0)
+			{
+				if (event.type == SDL_QUIT)
+				{
+					quit = true;
+				}
+			}
+			SDL_UpdateWindowSurface(window);
+		}
+	}
+	close();
 	return 0;
 }
